f5ac835cd7673e6c163d7451bf2854a803efdcd0
NicolasWinckler <n.winckler@gsi.de>
2014-10-04 03:10:30 +0200

    add Summary class

---
+++ OnlineAnalysis/VisualAnalysis/GUI/SidsSummary.cxx
@@ -0,0 +1,18 @@
+/* 
+ * File:   SidsSummary.cxx
+ * Author: winckler
+ * 
+ * Created on October 4, 2014, 3:09 AM
+ */
+
+#include "SidsSummary.h"
+
+SidsSummary::SidsSummary() {
+}
+
+SidsSummary::SidsSummary(const SidsSummary& orig) {
+}
+
+SidsSummary::~SidsSummary() {
+}
+
---
+++ OnlineAnalysis/VisualAnalysis/GUI/SidsSummary.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   SidsSummary.h
+ * Author: winckler
+ *
+ * Created on October 4, 2014, 3:09 AM
+ */
+
+#ifndef SIDSSUMMARY_H
+#define	SIDSSUMMARY_H
+
+class SidsSummary {
+public:
+    SidsSummary();
+    SidsSummary(const SidsSummary& orig);
+    virtual ~SidsSummary();
+private:
+
+};
+
+#endif	/* SIDSSUMMARY_H */
+
